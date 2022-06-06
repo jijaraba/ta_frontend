6f1dc24b04bebc8c2e714593fb3330f8508d0a74
José Jaraba <jijaraba@hotmail.com>
2022-06-06 01:47:04 -0500

    Change backend server

--- lib/src/config/config_develop.dart
+++ lib/src/config/config_develop.dart
@@ -2,7 +2,7 @@
 import 'package:tresastronautas_frotend/src/config/app_config.dart';
 
 const config = AppConfig(
-  serverBaseUrl: 'http://localhost:3000/api/v1',
+  serverBaseUrl: 'https://sea-lion-app-jjzri.ondigitalocean.app/api/v1',
   apiKey: '',
   appName: 'Tres Astronautas',
   policyUrl: 'https://www.tresastronautas.com/',
